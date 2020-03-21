1fd9abbb4178dbcac97e0066876ee94e279ef2cf
YeScraft <61934500+YeScraft@users.noreply.github.com>
2020-03-21 17:26:03 +0300

    Update A6_8.js

--- js/A6_8.js
+++ js/A6_8.js
@@ -1,20 +1,44 @@
+let progressBar = 0;
 
 function p1Change() {
-	$("#bar").attr({style: 'width: 1%'});
+	progressBar = progressBar + 1;
+	$("#bar").attr("style","width:"+progressBar+"%");
+	$("#bar").text(progressBar+"%");
+	if (progressBar>=100){
+		end();
+	}
 }
 
 function p3Change() {
-	$("#bar").attr({style: 'width: 3%'});
+	progressBar = progressBar + 3;
+	$("#bar").attr("style","width:"+progressBar+"%");
+	$("#bar").text(progressBar+"%");
+	if (progressBar>=100){
+		end();
+	}
 }
 function p7Change() {
-	$("#bar").attr({style: 'width: 7%'});
+	progressBar = progressBar + 7;
+	$("#bar").attr("style","width:"+progressBar+"%");
+	$("#bar").text(progressBar+"%");
+	if (progressBar>=100){
+		end();
+	}
+}
+
+function end() {
+	progressBar = 100;
+	$("#bar").text(progressBar+"%");
 }
 
 function p1() {
+
 	$("#p1").click(p1Change);
 	$("#p3").click(p3Change);
 	$("#p7").click(p7Change);
 	console.log("скрипт подгрузился");
+
+
 }
 
 $(document).ready(p1);
